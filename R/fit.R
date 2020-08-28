c4ea96c01759e274c4137d61d32ec9cff024cb0f
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2020-08-28 11:18:47 +0100

    port over rolling average and begin work on fit

---
+++ R/fit.R
@@ -0,0 +1,81 @@
+#' Fit a regression model
+#'
+#' @param dat An [incidence2::incidence] object.
+#' @param model The regression model to fit (can be "negbin" or "poisson").
+#' @param alpha Value of alpha used to calculate confidence intervals; defaults
+#'   to 0.05 which corresponds to a 95% confidence interval.
+#' @param ... Not currently used.
+#'
+#' @return
+#'   - `rolling_average` An [incidence2::incidence] like object with additional
+#'     fitted values and associated confidence intervals.
+#'
+#'
+#' @rdname fit
+#' @export
+fit <- function(dat, model, ...) {
+  UseMethod("fit")
+}
+
+#' @rdname fit
+#' @aliases fit.default
+#' @export
+fit.default <- function(dat, model, ...) {
+  stop(sprintf("Not implemented for class %s",
+               paste(class(dat), collapse = ", ")))
+}
+
+
+#' @rdname fit
+#' @aliases fit.incidence2
+#' @export
+fit.incidence2 <- function(dat, model = c("negbin", "poisson"), alpha = 0.05, ...) {
+
+  ellipsis::check_dots_empty()
+  model <- match.arg(model)
+  groups <- incidence2::get_group_names(dat)
+  out <- incidence2::as_tibble(dat)
+  dates <- incidence2::get_dates_name(dat)
+  count <- incidence2::get_counts_name(dat)
+  fmla <- stats::as.formula(paste(count, "~", dates))
+
+  if (!is.null(groups)) {
+    out <- dplyr::nest_by(grouped_df(out, groups))
+    out <- dplyr::mutate(
+      out,
+      model = list(
+        switch(
+          model,
+          negbin = MASS::glm.nb(fmla, data = .data$data),
+          poisson = stats::glm(fmla, data = .data$data, family = stats::poisson),
+          stop('Invalid model. Please use one of "negbin" or "poisson".')
+        )),
+      fitted = list(
+        fitted_values(.data$data, model, alpha)
+      ))
+  }
+  out$dat <- NULL
+
+  class(out) <- c("incidence_fit", class(out))
+
+  # copy over attributes
+  attr(out, "groups") <- groups
+  attr(out, "date") <- dates
+  attr(out, "count") <- count
+  attr(out, "interval") <- incidence2::get_interval(dat)
+  attr(out, "cumulative") <- attr(dat, "cumulative")
+  attr(out, "date_group") <- attr(dat, "date_group")
+
+
+  out
+}
+
+fitted_values <- function(x, fit, alpha) {
+  inverse_link <- stats::family(fit)$linkinv
+  pred <- stats::predict(fit, se.fit = TRUE)
+
+  x$fit <- inverse_link(pred$fit)
+  x$lower <- inverse_link(pred$fit + stats::qnorm(alpha/2) * pred$se.fit)
+  x$upper <- inverse_link(pred$fit + stats::qnorm(1 - alpha/2) * pred$se.fit)
+  x
+}
--- R/plot.R
+++ R/plot.R
@@ -1,37 +1,34 @@
-#' Add a rolling average to an incidence plot
+#' Plot a rolling average incidence object
 #'
-#' @param graph An incidence plot.
 #' @param x An `incidence2_ra` object created by [add_rolling_average()].
+#' @param ... Additional arguments to be passed to
+#'   [incidence2::plot.incidence2()] or [incidence2::facet_plot()].
 #'
-#' @return An incidence plot with the addition of a rolling average.
+#' @return An incidence plot with the addition of a rolling average.  This will
+#'   be facetted if the object is grouped.
 #'
 #' @importFrom rlang sym
 #' @export
-plot_rolling_average <- function(graph, x) {
-
-  if (!inherits(x, "incidence2_ra")) {
-    stop("x must be an object with class `incidence_ra`")
-  }
+plot.incidence2_ra <- function(x, ...) {
 
   ra <- attr(x, "rolling_average")
-  if (!is.null(ra)) {
-    shift <- mean(incidence2::get_interval(x, integer = TRUE))/2
+  groups <- incidence2::get_group_names(x)
+  date_var <- incidence2::get_dates_name(x)
+  shift <- mean(incidence2::get_interval(x, integer = TRUE))/2
 
-    date_var <- incidence2::get_dates_name(x)
-    graph <-
-      graph +
-      ggplot2::geom_point(mapping = ggplot2::aes(x = !!sym(date_var) + shift, y = !!sym(ra)),
-                          data = x,
-                          position = ggplot2::position_stack()) +
-      ggplot2::geom_line(mapping = ggplot2::aes(x = !!sym(date_var) + shift, y = !!sym(ra)),
-                         data = x,
-                         position = ggplot2::position_stack())
+  if (is.null(groups)) {
+    graph <- plot(x, ...)
+  } else {
+    graph <- incidence2::facet_plot(x, ...)
   }
 
-  graph
-
+  graph +
+    ggplot2::geom_point(mapping = ggplot2::aes(x = !!sym(date_var) + shift,
+                                               y = !!sym(ra)),
+                        data = x,
+                        position = ggplot2::position_stack()) +
+    ggplot2::geom_line(mapping = ggplot2::aes(x = !!sym(date_var) + shift,
+                                              y = !!sym(ra)),
+                       data = x,
+                       position = ggplot2::position_stack())
 }
-
-
-
-
