ae8408a1b20d598b40c37e81fb28ac332f326676
Thibaut Jombart <thibautjombart@gmail.com>
2021-03-22 18:50:42 +0100

    first complete version seems to work

--- R/flag_lowcounts.R
+++ R/flag_lowcounts.R
@@ -3,55 +3,112 @@
 #' Low counts may be genuine, but they can also reflect actually missing data or
 #' strong under-reporting. This function aims to detect the latter by flagging
 #' any count below a certain threshold, expressed as a fraction of the median
-#' count.
+#' count. Setting low values to NAs can be useful to help fitting temporal
+#' trends to the data, as zeros / low counts can throw off some models
+#' (e.g. Negative Binomial GLMs).
 #'
-#' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
+#' @author Tim Taylor and Thibaut Jombart
 #'
 #' @md
 #'
 #' @param x An [incidence2::incidence] object.
 #' 
-#' @param counts A `character` indicating the counts for which low values should
-#'   be flagged.
+#' @param counts A tidyselect compliant indication of the counts to be used.
 #'
-#' @param threshold
+#' @param threshold A numeric multiplier of the median count to be used as
+#'   threshold. Defaults to 0.01, in which case any count lower than 1% of the
+#'   median count is flagged as low count.
 #'
 #' @param set_missing A `logical` indicating if the low counts identified should
-#'   be replaced with NAs (`TRUE`, default). If `FALSE`, a new logical column called
-#'   `low_count` will be added, which will indicate which entries are flagg
+#'   be replaced with NAs (`TRUE`, default). If `FALSE`, new logical columns
+#'   with the `flag_low` suffix will be added, indicating which entries are
+#'   below the threshold.
 #' 
 #' @return An [incidence2::incidence] object.
 #'
 #' @export
+#' @importFrom dplyr grouped_df mutate across if_else
+#'
+#' @examples
 #' 
-flag_lowcounts <- function(x, counts, threshold = 0.01) {
+#' if (requireNamespace("outbreaks", quietly = TRUE) &&
+#'     requireNamespace("incidence2", quietly = TRUE)) {
+#'   data(covid19_england_nhscalls_2020, package = "outbreaks")
+#'   dat <- covid19_england_nhscalls_2020
+#'   i <- incidence(dat, date, interval = "monday week", count = count)
+#'   plot(i)
+#'   plot(flag_low_counts(i, threshold = 0.1))
+#'   plot(flag_low_counts(i, threshold = 1), title = "removing counts below the median")
+#' }
+
+
+flag_low_counts <- function(x, counts = NULL, threshold = 0.01, set_missing = TRUE) {
+  
+  ## checks
   if (!inherits(x, "incidence2")) {
     stop(sprintf("`%s` is not an incidence object", deparse(substitute(x))))
   }
-  x_attr <- attributes(x)
 
-  if (missing(counts)) {
-    counts <- incidence2::get_count_names(x)[1]
+  ## snapshot original attributes
+  original_attributes <- attributes(x)
+
+  ## if no count is given use the current counts
+  counts <- rlang::enquo(counts)
+  if (!rlang::quo_is_null(counts)) {
+    idx <- tidyselect::eval_select(counts, x, allow_rename = FALSE)
+    counts <- names(x)[idx]
+  } else {
+    counts <- NULL
   }
-
-  count_names <- incidence2::get_count_names(x)
-  group_names <- incidence2::get_group_names(x)
-
-  if (!counts %in% count_names) {
-    msg <- sprintf("`%s` is not a count variable of `x` (acceptable: %s)",
-                   counts,
-                   paste(count_names, collapse = ", "))
-    stop(msg)
+  if (is.null(counts)) {
+    counts <- get_count_names(x)
   }
-
-  out <- dplyr::group_by(x, dplyr::.data[[group_names]])
-  out <- dplyr::mutate(out,
-                       low_counts_ = .data[[counts]] < threshold * median(.data[[counts]], na.rm = TRUE))
-
-  ## restore attributes and return
-  onames <- names(out)
-  attributes(out) <- x_attr
-  names(out) <- onames
-  out
   
+  ## get group and date names
+  group_names <- get_group_names(x)
+
+  ## The output can take two forms depending on `set_missing`:
+  ## * TRUE: counts are modified so that values below the threshold are set to NA
+  ## * FALSE: counts are not modified, but new logical variables with a "flag_low"
+  ## suffix are generated, with TRUE wherever values are below the threshold
+ 
+  below_thres <- function(x) {
+    x < threshold * median(x, na.rm = TRUE)
+  }
+
+  out <- x
+  if (!is.null(group_names)) {
+    out <- grouped_df(out, group_names)
+  }
+
+  if (set_missing) {
+    out <- mutate(
+        out,
+        across(
+        {{counts}},
+        function(x) if_else(
+                        below_thres(x),
+                        NA_integer_,
+                        x
+                    )
+        )
+    )
+  } else {
+    new_var_names <- paste(counts, "flag_low", sep = "_")
+    out <- mutate(
+        out,
+        across(
+        {{counts}},
+        below_thres,
+        .names = new_var_names
+        )
+    )
+  }
+
+  ## restore attributes
+  new_names <- names(out)
+  attributes(out) <- original_attributes
+  names(out) <- new_names
+ 
+  out
 }
