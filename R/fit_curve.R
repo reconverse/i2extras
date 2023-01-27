5d8ee0bdcd35e1bbcec47db045776666929cf6e9
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2023-01-27 13:16:35 +0000

    update curve fitting and plot due to incidence2 changes

--- R/fit_curve.R
+++ R/fit_curve.R
@@ -15,104 +15,86 @@
 #' @importFrom tidyr pivot_longer
 #' @export
 fit_curve <- function(x, model, ...) {
-  UseMethod("fit_curve")
+    UseMethod("fit_curve")
 }
 
 #' @rdname fit_curve
 #' @aliases fit_curve.default
 #' @export
 fit_curve.default <- function(x, model, ...) {
-  not_implemented(x)
+    not_implemented(x)
 }
 
 
 #' @rdname fit_curve
 #' @aliases fit_curve.incidence2 incidence2_fit
 #' @export
-fit_curve.incidence2 <- function(x,
-                                 model = c("poisson", "negbin"),
-                                 alpha = 0.05,
-                                 ...) {
+fit_curve.incidence2 <- function(
+        x,
+        model = c("poisson", "negbin"),
+        alpha = 0.05,
+        ...
+) {
 
-  # ensure model is poisson or negbin
-  model <- match.arg(model, several.ok = TRUE)
+    # ensure model is poisson or negbin
+    model <- match.arg(model)
 
-  # get other variable names
-  group_vars <- incidence2::get_group_names(x)
-  count_vars <- incidence2::get_counts_name(x)
-  date_var <- incidence2::get_dates_name(x)
+    # get other variable names
+    group_vars <- get_group_names(x)
+    count_var <- get_count_variable_name(x)
+    count_val <- get_count_value_name(x)
+    date_var <- get_date_index_name(x)
 
-  # fix for global variable warning
-  dat <- NULL
+    # fix for global variable warning
+    dat <- NULL
 
-  # ensure model is of length one or the same length as counts_var
-  if (length(model) == 1) model <- rep(model, length(count_vars))
-  if (length(model) != length(count_vars)) {
-    stop("model must be of length 1 or the same length as `count_var`")
-  }
-  model <- setNames(model, count_vars)
+    # nest by count_variable and group_vars
+    grouping_variables <- c("count_variable", group_vars)
+    out <- dplyr::nest_by(grouped_df(x, grouping_variables), .key = "data")
 
-  # melt data for convenience
-  out <- tidyr::pivot_longer(
-    x,
-    cols = count_vars,
-    values_drop_na = TRUE,
-    names_to = "count_variable",
-    values_to = "count"
-  )
+    # perform fitting and capture any warnings / errors
+    fiterr <- lapply(
+        out$data,
+        function(dat, cnt) {
+            fmla <- stats::as.formula(paste(count_val, "~", date_var))
+            trending_model <- switch(
+                model,
+                negbin = trending::glm_nb_model(fmla, ...),
+                poisson = trending::glm_model(fmla, family = "poisson", ...),
+                stop('Invalid model. Please use one of "negbin" or "poisson".')
+            )
+            trending::fit(trending_model, dat, as_tibble = TRUE)
+        }
+    )
 
-  # nest by count_variable and group_vars
-  grouping_variables <- c("count_variable", group_vars)
-  out <- dplyr::nest_by(grouped_df(out, grouping_variables), .key = "data")
+    # perform prediction and capture any warnings / errors
+    prederr <- lapply(fiterr, stats::predict, as_tibble = TRUE)
+    prederr <- do.call(rbind, prederr)
+    fiterr <- do.call(rbind, fiterr)
 
-  # perform fitting and capture any warnings / errors
-  fiterr <- mapply(
-    function(dat, cnt) {
-      mdl <- model[[cnt]]
-      fmla <- stats::as.formula(paste("count", "~", date_var))
-      trending_model <- switch(
-        mdl,
-        negbin = trending::glm_nb_model(fmla, ...),
-        poisson = trending::glm_model(fmla, family = "poisson", ...),
-        stop('Invalid model. Please use one of "negbin" or "poisson".')
-      )
-      trending::fit(trending_model, dat, as_tibble = TRUE)
-    },
-    out$data,
-    count_vars,
-    SIMPLIFY = FALSE
-  )
+    # add columns to output
+    out$model <- fiterr[[1]]
+    out$estimates <- prederr[[1]]
+    out$fitting_warning <- fiterr[[2]]
+    out$fitting_error <- fiterr[[3]]
+    out$prediction_warning <- prederr[[2]]
+    out$prediction_error <- prederr[[3]]
+    warning_vars <- c("fitting_warning", "prediction_warning")
+    error_vars <- c("fitting_error", "prediction_error")
 
-  # perform prediction and capture any warnings / errors
-  prederr <- lapply(fiterr, stats::predict, as_tibble = TRUE)
-  prederr <- do.call(rbind, prederr)
-  fiterr <- do.call(rbind, fiterr)
-
-  # add columns to output
-  out$model <- fiterr[[1]]
-  out$estimates <- prederr[[1]]
-  out$fitting_warning <- fiterr[[2]]
-  out$fitting_error <- fiterr[[3]]
-  out$prediction_warning <- prederr[[2]]
-  out$prediction_error <- prederr[[3]]
-  warning_vars <- c("fitting_warning", "prediction_warning")
-  error_vars <- c("fitting_error", "prediction_error")
-
-  # TODO - review which attributes are actually necessary
-  # output a subclass of tibble
-  out <- tibble::new_tibble(out,
-                            groups = group_vars,
-                            date = date_var,
-                            count_variable = "count_variable",
-                            counts = count_vars,
-                            data = "data",
-                            interval = incidence2::get_interval(x),
-                            cumulative = attr(dat, "cumulative"),
-                            model = "model",
-                            fitted = "estimates",
-                            warning_vars = warning_vars,
-                            error_vars = error_vars,
-                            nrow = nrow(out),
-                            class = "incidence2_fit")
-  tibble::validate_tibble(out)
+    # TODO - review which attributes are actually necessary
+    # output a subclass of tibble
+    out <- tibble::new_tibble(out,
+                              groups = group_vars,
+                              date = date_var,
+                              count_variable = count_var,
+                              counts = count_val,
+                              data = "data",
+                              model = "model",
+                              fitted = "estimates",
+                              warning_vars = warning_vars,
+                              error_vars = error_vars,
+                              nrow = nrow(out),
+                              class = "incidence2_fit")
+    tibble::validate_tibble(out)
 }
--- R/plot_fit.R
+++ R/plot_fit.R
@@ -3,17 +3,30 @@
 #' @author Tim Taylor
 #'
 #' @param x An `incidence2_fit` object created by [fit_curve()].
-#' @param cnt The count variable to print.  If `NULL` defaults to the first
-#'   value from `attr(x, "counts")`.
-#' @param include_warnings Include results in plot that triggered warnings but
-#'   not errors.  Defaults to `FALSE`.
-#' @param ci Plot confidence intervals (defaults to TRUE).
-#' @param pi Plot prediction intervals (defaults to FALSE).
-#' @param ... Additional arguments to be passed to
-#'   [incidence2::plot.incidence2()] or [incidence2::facet_plot()].
 #'
-#' @return An incidence plot with the addition of a fitted curve.  This will
-#'   be facetted if the object is grouped.
+#' @param include_warnings
+#'
+#' Include results in plot that triggered warnings but not errors.
+#'
+#' Defaults to `FALSE`.
+#'
+#' @param ci
+#'
+#' Plot confidence intervals.
+#'
+#' Defaults to TRUE.
+#'
+#' @param pi
+#'
+#' Plot prediction intervals.
+#'
+#' Defaults to FALSE.
+#'
+#' @param ...
+#'
+#' Additional arguments to be passed to [incidence2::plot.incidence2()] .
+#'
+#' @return An incidence plot with the addition of a fitted curve.
 #'
 #' @importFrom rlang sym
 #' @importFrom tidyr pivot_wider unnest
@@ -27,72 +40,41 @@ plot.incidence2_fit <- function(x, cnt = NULL, include_warnings = FALSE,
   date_var <- attr(x, "date")
   count_variable <- attr(x, "count_variable")
   counts <- attr(x, "counts")
-  interval <- attr(x, "interval")
-  cumulative <- attr(x, "cumulative")
-  data_var <- attr(x, "data")
-  model_var <- attr(x, "model")
-
-  # filter to desired count or default to first one
-  if (is.null(cnt)) {
-    cnt <- counts[1]
-  }
-  if (!cnt %in% counts) {
-    stop(
-      sprintf("`%s` is not a valid count variable", deparse(substitute(cnt)))
-    )
-  }
-  x <- x[x[[count_variable]] == cnt, ]
+  estimates <- attr(x, "fitted")
 
   # filter results to exclude errors and, optionally, warnings
   x <- is_ok(x, include_warnings = include_warnings)
 
-  # remove unnecesary columns
-  x[[model_var]] <- NULL
-  x[[data_var]] <- NULL
+  # convert to incidence object
+  x <- x[, c(count_variable, group_vars, estimates)]
+  x <- unnest(x, all_of(estimates))
 
-  # unnest and widen data
-  dat <- unnest(x, tidyr::everything())
-  dat <- pivot_wider(dat, names_from = count_variable, values_from = "count")
-
-
-
-  # create an incidence object to help with plotting
-  dat <- minimal_incidence(
-    dat,
-    groups = group_vars,
-    date = date_var,
-    counts = cnt,
-    interval = interval,
-    cumulative = cumulative
+  # convert to incidence
+  x <- structure(
+      x,
+      date_index = date_var,
+      count_variable = count_variable,
+      count_value = counts,
+      groups = group_vars,
+      class = c("incidence2", "data.frame")
   )
 
-  # create single or facet_plot as appropriate
-  if (is.null(group_vars)) {
-    graph <- plot(dat, ...)
-  } else {
-    graph <- incidence2::facet_plot(dat, ...)
-  }
-
-  # Adjustment due to how dates are plotted (centred)
-  if (inherits(dat[[date_var]], "period")) {
-    shift <- mean(incidence2::get_interval(dat, integer = TRUE))/2
-  } else {
-    shift <-0
-  }
+  # plot
+  graph <- plot(x, ...)
 
   # ribbon colour
   col_model <- "#BBB67E"
 
   # add estimate (prediction) to plot
-  graph <- graph +
+ graph <- graph +
     ggplot2::geom_line(
-      mapping = ggplot2::aes(x = !!sym(date_var) + shift, y = .data$estimate)
+      mapping = ggplot2::aes(x = !!sym(date_var), y = .data$estimate)
     )
 
   # add confidence intervals to plot
   if (ci) {
     graph <- graph +
-      ggplot2::geom_ribbon(ggplot2::aes(x = !!sym(date_var) + shift,
+      ggplot2::geom_ribbon(ggplot2::aes(x = !!sym(date_var),
                                       ymin = .data$lower_ci,
                                       ymax = .data$upper_ci),
                            alpha = 0.5,
@@ -102,7 +84,7 @@ plot.incidence2_fit <- function(x, cnt = NULL, include_warnings = FALSE,
   # add prediction intervals to plot
   if (pi) {
     graph <- graph +
-      ggplot2::geom_ribbon(ggplot2::aes(x = !!sym(date_var) + shift,
+      ggplot2::geom_ribbon(ggplot2::aes(x = !!sym(date_var),
                                       ymin = .data$lower_pi,
                                       ymax = .data$upper_pi),
                            alpha = 0.3,
